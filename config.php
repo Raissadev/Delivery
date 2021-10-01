119242fa580dac16748bda2a6dd07d4cb550ae30
Raissadev <82960240+Raissadev@users.noreply.github.com>
2021-09-30 21:48:51 -0300

    Update in config

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 
 	date_default_timezone_set('America/Sao_Paulo');
 
-	define('INCLUDE_PATH','http://192.168.0.107/myCode/Back-end/Projetos_Back/Delivery/');
+	define('INCLUDE_PATH','http://localhost/');
 	define('HOST','localhost');
 	define('USER','root');
 	define('PASSWORD','');
-	define('DATABASE','delivery_mycode');
+	define('DATABASE','database');
 
 ?>
